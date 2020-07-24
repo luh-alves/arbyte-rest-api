46eb7d9ebecd623aa488b0ec9a772a1e2dbb355a
Luciana Alves <luh.ana.af@gmail.com>
2020-07-24 20:21:36 -0300

    add-procfile

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -28,18 +28,15 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       tableName: 'knex_migrations'
-    }
+    },
+    useNullAsDefault: true,
   }
 
 };
